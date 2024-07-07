7e6b2dd4c9075a57d8e57322b0465f4a0f494467
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-06 18:33:34 -0700

    created getUsers function for returning all users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from 'express'
 import { User } from '../db/models'
 
+
+const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({})
+    return res.send(users)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const userById = async (req: Request, res: Response) => {
 	try {
 		const user = await User.findById(req.params.id)
@@ -10,6 +20,20 @@ const userById = async (req: Request, res: Response) => {
 	}
 }
 
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const body = await req.body
+    const user = await User.create({
+      ...body
+    })
+    return res.send(user)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export default { 
-  userById
+  getUsers,
+  userById,
+  createUser
 }
