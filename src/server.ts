a89bd1f201e797aa3433bfe7896bb783203e0ea6
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-14 23:42:29 -0700

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import './db/connection'
 import routes from './router'
 import { generateRoutes } from './helpers/generateRoutes'
@@ -11,6 +12,7 @@ const app: Express = express()
 const port = process.env.PORT || 3001
 
 // Middleware
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
