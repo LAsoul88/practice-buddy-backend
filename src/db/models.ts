f41b2ddb5245a7037f0f96c92065a559d597d5aa
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-17 11:27:19 -0700

    readded name as username to user Schema

--- src/db/models.ts
+++ src/db/models.ts
@@ -2,15 +2,19 @@ import mongoose, { Schema } from 'mongoose'
 const ObjectId = Schema.ObjectId
 
 const userSchema = new Schema({
-  password: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true,
     unique: true
   },
+  password: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  }
 })
 
 const User = mongoose.model('users', userSchema)
