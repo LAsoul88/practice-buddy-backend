13a58f67f88b24f7f504750e350f399c838fd846
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-07-25 13:12:04 -0700

    adjusted routes to correctly utilize verifyToken middleware

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -4,7 +4,7 @@ import { verifyToken } from '../helpers/authenticate'
 
 const router = express.Router()
 
-router.post('/login', verifyToken, controllers.auth.login)
+router.post('/login', controllers.auth.login)
 router.post('/register', controllers.auth.register)
 
 export default router
--- src/router/entries.ts
+++ src/router/entries.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import controllers from '../controllers'
+import { verifyToken } from '../helpers/authenticate'
 
 const router = express.Router()
 
-router.get('/', controllers.entries.entriesByUser)
-router.post('/', controllers.entries.createEntry)
+router.get('/', verifyToken, controllers.entries.entriesByUser)
+router.post('/', verifyToken, controllers.entries.createEntry)
 router.delete('/', controllers.entries.deleteEntries)
 
 export default router
