5fd99566628c03c5b6471c9a5892e3a0b82a6ef1
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-05 15:14:30 -0700

    added cors config to server

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,18 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT || 3001
 
+const corsConfig = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+}
+
 // Middleware
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(cors(corsConfig))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
 app.use((req, res, next) => {
   console.log(req.url)
   next()
