d31929f318fe21766d0144cdb62ed1935c3e8bc1
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-12 15:59:33 -0700

    server file cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import './db/connection'
@@ -15,7 +14,6 @@ const port = process.env.PORT || 3001
 const corsConfig = {
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   credentials: true
 }
 
@@ -24,13 +22,13 @@ app.use(cors(corsConfig))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use((req, res, next) => {
   console.log(req.method, req.url)
-  console.log('cookies: ', req.headers)
+  console.log('cookies: ', req.cookies)
   next()
 })
 
+// generates all routes dynamically
 generateRoutes(routes, app)
 
 app.get('/', (req: Request, res: Response) => {
